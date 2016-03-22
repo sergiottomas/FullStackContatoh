dd5969be6510bfbae4d4b70e1b5f7b8cc59a1a23
Sergio Tomas <sergio.souza@tugare.com.br>
2016-03-22 18:53:06 -0300

    teste 11

--- config/database.js
+++ config/database.js
@@ -1,24 +1,25 @@
 var mongoose = require('mongoose');
 
-module.exports = function(uri){
-  mongoose.connect(uri, {server: {poolSize: 15}});
+module.exports = function(uri) {
+	
+	mongoose.connect(uri);
 
-  mongoose.connection.on('connected', function(){
-    console.log('Mongoose! Conectado em ' + uri);
-  });
+	mongoose.connection.on('connected', function() {
+	  console.log('Mongoose! Conectado em ' + uri);
+	});
 
-  mongoose.connection.on('disconnected', function(){
-    console.log('Mongoose! Desconectado de ' + uri);
-  });
+	mongoose.connection.on('disconnected', function() {
+	  console.log('Mongoose! Desconectado de ' + uri);
+	});
 
-  mongoose.connection.on('error', function(error){
-    console.log('Mongoose! Erro na conexão: ' + uri);
-  });
+	mongoose.connection.on('error', function(erro) {
+	  console.log('Mongoose! Erro na conexão: ' + erro);
+	});
 
-  process.on('SIGINT', function(){
-    mongoose.connection.close(function(){
-      console.log('Mongoose! Desconectado pelo término da aplicação');
-      process.exit(0);
+	process.on('SIGINT', function() {
+  	  mongoose.connection.close(function() {
+        console.log('Mongoose! Desconectado pelo término da aplicação');
+        process.exit(0);
+      });
     });
-  });
 }
