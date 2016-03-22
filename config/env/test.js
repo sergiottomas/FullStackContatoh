6aa09bb953b33ddbde78231f216a1eb1f03f2d75
Sergio Tomas <sergio.souza@tugare.com.br>
2016-03-22 17:25:09 -0300

    Commit de verificacao

--- config/env/test.js
+++ config/env/test.js
@@ -2,12 +2,12 @@ module.exports = {
   env: 'test',
   db: 'mongodb://localhost/contatooh_test',
   sauceTestName: 'Contatooh E2E Testing',
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  sauceUser : process.env.SAUCE_USERNAME, 
+  sauceKey : process.env.SAUCE_ACCESS_KEY, 
   travisJobNumber: process.env.TRAVIS_JOB_NUMBER,
   travisBuild: process.env.TRAVIS_BUILD_NUMBER,
-  clientID: process.env.CLIENT_ID,
+  clientID: process.env.CLIENT_ID, 
   clientSecret: process.env.CLIENT_SECRET,
-  seleniumUser: process.env.SELENIUM_USER,
+  seleniumUser: process.env.SELENIUM_USER, 
   seleniumUserPassword: process.env.SELENIUM_USER_PASSWORD
-}
+};
