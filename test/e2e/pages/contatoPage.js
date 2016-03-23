419b409e4087fb23e958e09ffa9a5e1da2972f7e
Sergio Tomas <sergio.souza@tugare.com.br>
2016-03-23 12:15:19 -0300

    Correção Nova 2

--- test/e2e/pages/contatoPage.js
+++ test/e2e/pages/contatoPage.js
@@ -1,27 +1,28 @@
-var ContatoPage = function(){
-  this.visitar = function(){
-    browser.get('http://localhost:3000/#/contato');
-  };
 
-  this.digitarNome = function(nome){
-    element(by.model('contato.nome')).sendKeys(nome);
-  };
+var contatoPage = function() {
 
-  this.digitarEmail = function(email){
-    element(by.model('contato.email')).sendKeys(email);
-  };
+    this.visitar = function() {
+        browser.get('http://localhost:3000/#/contato');
+    };
 
-  this.salvar = function(){
-    element(by.css('.btn-primary')).click();
-  };
+    this.digitarNome = function(nome) {
+         element(by.model('contato.nome')).sendKeys(nome);
+    };
 
-  this.obterMensagem = function(){
-    return element(by.binding('mensagem.texto')).getText();
-  };
+    this.digitarEmail = function(email) {
+         element(by.model('contato.email')).sendKeys(email);
+    };
 
-  this.selecionarNomePrimeiraEmergenciaDaLista = function(){
-    element(by.css('option[value="0"]')).click();
-  };
+    this.salvar = function() {
+        element(by.css('.btn-primary')).click();
+    };
+
+    this.obterMensagem = function() {
+        return element(by.binding('mensagem.texto')).getText()
+    };
+    
+    this.selecionarPrimeiraEmergenciaDaLista = function() {
+        element(by.css('option[value="0"]')).click();
+    };  
 }
-
-module.exports = ContatoPage;
+module.exports = contatoPage;
--- test/e2e/pages/contatosPage.js
+++ test/e2e/pages/contatosPage.js
@@ -1,19 +1,24 @@
-var ContatosPage = function(){
-  this.visitar = function(){
-    browser.get('http://localhost:3000/#/contatos');
-  };
+var contatosPage = function() {
 
-  this.obterUsuarioLogado = function(){
-    return element(by.css('#usuario-logado')).getText();
-  };
+    this.visitar = function() {
+        browser.get('http://localhost:3000/#/contatos');
+    };
 
-  this.obterTotalDeItensDaLista = function(){
-    return element.all(by.repeater('contato in contatos')).count();
-  };
+    this.obterUsuarioLogado = function(nome) {
+         return element(by.id('usuario-logado')).getText();
+    };
 
-  this.removePrimeiroItemDaLista = function(){
-    element(by.repeater('contato in contatos').row(0)).element(by.css('.btn')).click();
-  };
+    this.obterTotalDeItensDaLista = function() {
+
+        return element.all(by.repeater('contato in contatos'))
+        			  .count();
+    };
+
+    this.removerPrimeiroItemDaLista = function() {
+
+         element(by.repeater('contato in contatos').row(0))
+            .element(by.css('.btn'))
+            .click();
+    }
 }
-
-module.exports = ContatosPage;
+module.exports = contatosPage;
