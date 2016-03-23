293168860c30b599dc82b915cb1f4b3d21c8d5f2
Sergio Tomas <sergio.souza@tugare.com.br>
2016-03-23 12:21:56 -0300

    Novo teste

--- test/e2e/ContatosPageSpec.js
+++ test/e2e/ContatosPageSpec.js
@@ -1,25 +1,23 @@
 var ContatosPage = new require('./pages/contatosPage');
 
-describe('Página principal', function() {
+describe('Página Principal', function(){
+  var pagina = new ContatosPage();
 
-    var pagina = new ContatosPage();
+  beforeEach(function(){
+    pagina.visitar();
+  });
 
-    beforeEach(function() {
-        pagina.visitar();
+  //spec
+  it('Deve estar logado', function(){
+    pagina.obterUsuarioLogado().then(function(texto){
+      expect(texto.trim().length).toBeGreaterThan(0);
     });
+  });
 
-    it('Deve estar logado', function() {
-
-        pagina.obterUsuarioLogado().then(function(texto) {
-            expect(texto.trim().length).toBeGreaterThan(0);
-        });
-    });
-
-    it('Deve remover um contato da lista', function() {
-
-        var totalAntes = pagina.obterTotalDeItensDaLista();
-        pagina.removerPrimeiroItemDaLista();
-        var totalDepois = pagina.obterTotalDeItensDaLista();
-        expect(totalDepois).toBeLessThan(totalAntes);
-    });
+  it('Deve remover um contato da lista', function(){
+    var totalAntes = pagina.obterTotalDeItensDaLista();
+    pagina.removePrimeiroItemDaLista();
+    var totalDepois = pagina.obterTotalDeItensDaLista();
+    expect(totalDepois).toBeLessThan(totalAntes);
+  });
 });
